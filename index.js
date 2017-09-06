28cb7991c24f0d5c191598ac52be08fb8b436ba8
Kenneth Cravens <bombfodder93@gmail.com>
2017-09-06 16:12:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ const two = 2;
 
 }
 
-var funkyFunction = function()
-  {
+var funkyFunction = function() {
+  return function() {
     return "FUNKY!"
   }
-
+}
 
 // We want 'funkyFunction' on the line below to return a function that returns "FUNKY!" -- how can we accomplish that?
 // NOTE: To pass this final test, you only need to modify the code below this line.
